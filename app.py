f90db933d7ba05e7935f1d842465ad2f8e32d96e
Mark Briscoe <mark@mcmw.co.uk>
2025-02-18 09:31:32 +0000

    removed colorama style module

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # import packages
 import os
 import random
-from colorama import Fore, Style
+from colorama import Fore
 
 os.system("clear")
 
@@ -48,7 +48,7 @@ deck_of_cards = create_deck()
 
 # game loop
 while len(deck_of_cards) > 1:
-    input("press enter to deal cards")
+    input(f"{Fore.WHITE}\npress enter to deal cards")
 
     os.system("clear")
 
@@ -68,13 +68,11 @@ while len(deck_of_cards) > 1:
     # if player 1 wins
     if player1_card[0] > player2_card[0]:
         # display cards
-        print(
-            f"{Fore.GREEN}Player 1 card: {p1_card_name} of {player1_card[1]}")
+        print(f"{Fore.GREEN}Player 1 card: {p1_card_name} of {player1_card[1]}")
         print(f"{Fore.RED}Player 2 card: {p2_card_name} of {player2_card[1]}")
-        print(Style.RESET_ALL)
 
         # display winner
-        print(f"{Fore.YELLOW}Player 1 wins this hand!{Style.RESET_ALL}")
+        print(f"{Fore.YELLOW}Player 1 wins this hand!")
 
         # add to winning pack
         player1_pack.append(player1_card)
@@ -84,12 +82,10 @@ while len(deck_of_cards) > 1:
     elif player1_card[0] < player2_card[0]:
         # display cards
         print(f"{Fore.RED}Player 1 card: {p1_card_name} of {player1_card[1]}")
-        print(
-            f"{Fore.GREEN}Player 2 card: {p2_card_name} of {player2_card[1]}")
-        print(Style.RESET_ALL)
+        print(f"{Fore.GREEN}Player 2 card: {p2_card_name} of {player2_card[1]}")
 
         # display winner
-        print(f"{Fore.YELLOW}Player 2 wins this hand!{Style.RESET_ALL}")
+        print(f"{Fore.YELLOW}Player 2 wins this hand!")
 
         # add to winning pack
         player2_pack.append(player1_card)
@@ -99,57 +95,40 @@ while len(deck_of_cards) > 1:
     else:
         # display cards
         print(f"{Fore.RED}Player 1 card: {p1_card_name} of {player1_card[1]}")
-        print(
-            f"{Fore.GREEN}Player 2 card: {p2_card_name} of {player2_card[1]}")
-        print(Style.RESET_ALL)
+        print(f"{Fore.GREEN}Player 2 card: {p2_card_name} of {player2_card[1]}")
 
         # display result
-        print(f"{Fore.YELLOW}It's a draw!{Style.RESET_ALL}")
+        print(f"{Fore.YELLOW}It's a draw!")
 
         # add to winning pack
         player1_pack.append(player1_card)
         player2_pack.append(player2_card)
 
     # display number of cards left in deck
-    print(
-        f"\n{Fore.BLUE} Number of cards left: {len(deck_of_cards)}{Style.RESET_ALL}")
+    print(f"\n{Fore.BLUE} Number of cards left: {len(deck_of_cards)}")
 # game loop ends
 
 # determine the winner
 if len(player1_pack) > len(player2_pack):
     # player 1 wins
-    print(
-        f"{Fore.YELLOW}Player 1 Wins the game with {len(player1_pack)} cards over {
-            len(player2_pack)} cards!"
-    )
+    print(f"{Fore.YELLOW}Player 1 Wins the game with {len(player1_pack)} cards over {len(player2_pack)} cards!")
 elif len(player1_pack) < len(player2_pack):
     # player2 wins
-    print(
-        f"{Fore.YELLOW}Player 2 Wins the game with {len(player2_pack)} cards over {
-            len(player1_pack)} cards!"
-    )
+    print(f"{Fore.YELLOW}Player 2 Wins the game with {len(player2_pack)} cards over {len(player1_pack)} cards!")
 else:
     # it's a draw
     print(f"{Fore.YELLOW}It's a draw!!")
 
-print(Style.RESET_ALL)
 
-
-if input("would you like to see the player's packs (y/n)?").upper() == "Y":
+if input(f"{Fore.WHITE}\nwould you like to see the player's packs (y/n)?").upper() == "Y":
     # display the cards in the player packs
     for card in player1_pack:
-        print(
-            f"{Fore.MAGENTA}Player 1 Pack: {process_card(card[0])} of {card[1]}{
-                Style.RESET_ALL}"
-        )
+        print(f"{Fore.MAGENTA}Player 1 Pack: {process_card(card[0])} of {card[1]}")
 
     for card in player2_pack:
-        print(
-            f"{Fore.CYAN}Player 2 Pack: {process_card(card[0])} of {card[1]}{
-                Style.RESET_ALL}"
-        )
+        print(f"{Fore.CYAN}Player 2 Pack: {process_card(card[0])} of {card[1]}")
 
-    print("\nThanks for playing!\n")
+    print(f"{Fore.WHITE}\nThanks for playing!\n")
 else:
 
-    print("\nThanks for playing!\n")
+    print(f"{Fore.WHITE}\nThanks for playing!\n")
